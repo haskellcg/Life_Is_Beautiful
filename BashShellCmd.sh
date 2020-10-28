ae91b3424c3bd7ad40319291262922d6e1aa34c5
haskellcg <haskellcg@foxmail.com>
2020-10-28 16:51:01 +0800

    add Bash shell command file

---
+++ BashShellCmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://www.runoob.com/linux/linux-shell-variable.html
+
+echo "hello world"
+
+yourname="dead by daylight"
+echo $yourname
+
+for skill in Ada Coffe Action Java; do
+    echo "I am good at ${skill}Script"
+done
